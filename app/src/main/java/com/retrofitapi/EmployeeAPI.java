947fe8a5f21f7faad5ad5da2ab508aec08315e6a
Softwarica College Lab <softwarica@softwarica.com>
2019-05-12 11:15:21 +0545

    EmployeeCUD class created

--- app/src/main/java/com/retrofitapi/EmployeeAPI.java
+++ app/src/main/java/com/retrofitapi/EmployeeAPI.java
@@ -6,9 +6,13 @@ import model.Employee;
 import retrofit2.Call;
 import retrofit2.CallAdapter;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface EmployeeAPI {
 
     @GET("employees")
     Call<List<Employee>>getEmployee();
+
+    @GET("employee/{empID}")
+    Call<Employee> getEmployeeByID(@Path("empID") int empID);
 }
