aca493fb88f04a56c3bb314d5899bdf6eae8d94c
Softwarica College Lab <softwarica@softwarica.com>
2019-05-12 11:42:32 +0545

    Registered

--- app/src/main/java/com/retrofitapi/RegisterEmployeeActivity.java
+++ app/src/main/java/com/retrofitapi/RegisterEmployeeActivity.java
@@ -5,8 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import model.EmployeeCUD;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RegisterEmployeeActivity extends AppCompatActivity {
 
@@ -38,6 +44,30 @@ public class RegisterEmployeeActivity extends AppCompatActivity {
         String name=etName.getText().toString();
         Float salary=Float.parseFloat(etSalary.getText().toString());
         int age=Integer.parseInt(etAge.getText().toString());
+
+        EmployeeCUD employee= new EmployeeCUD(name, salary, age);
+
+        Retrofit retrofit=new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        EmployeeAPI employeeAPI=retrofit.create(EmployeeAPI.class);
+
+        Call<Void> voidCall =employeeAPI.registerEmployee(employee);
+
+        voidCall.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Toast.makeText(RegisterEmployeeActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(RegisterEmployeeActivity.this, "Error", Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 }
